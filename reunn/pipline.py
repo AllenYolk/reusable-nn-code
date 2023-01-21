362d997064f55fc1c9d6fb55965697c7b0796640
AllenYolk <huang2627099045@gmail.com>
2023-01-22 00:31:31 +0800

    chore: modify requirements.txt, and add basic pipeline interface.

--- reunn/pipline.py
+++ reunn/pipline.py
@@ -1,5 +1,10 @@
 class TaskPipeline:
 
-    def __init__(self):
-        pass
+    def __init__(self, imp):
+        self._imp = imp
 
+
+class SupervisedTaskPipeline(TaskPipeline):
+
+    def __init__(self, backend: str = "torch"):
+        pass

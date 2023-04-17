e9091d013964050a31cbef10e015b5cff6f12efb
AllenYolk <allen.yfhuang@gmail.com>
2023-04-17 11:43:05 +0800

    docs: upgrade version to 0.2.0 .

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("./README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="reunn",
-    version="0.1.0",
+    version="0.2.0",
     author=(
         "AllenYolk (Yifan Huang, from Peking University); "
         "RadonTRD (Ruidong Tang, from Donghua University)"
